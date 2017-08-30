cd3298aa49874d7605c2ecca992d0a6ac8df452c
Cassius Vinicius de Magalhães <cassius@cvm.eti.br>
2017-08-29 23:09:50 -0300

    Injeção de dependências pelo Construtor e Setter.

--- demo/src/main/java/demo/DemoApplication.java
+++ demo/src/main/java/demo/DemoApplication.java
@@ -7,9 +7,15 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
-        HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
-        helloWorld.sayHello();
-    }
+	public static void main(String[] args) {
+		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+
+		InjectedByConstructorService constructorService = (InjectedByConstructorService) ctx
+				.getBean("injectedByConstructorService");
+		constructorService.getMessage();
+
+		InjectedBySetterService injectedBySetterService = (InjectedBySetterService) ctx
+				.getBean("injectedBySetterService");
+		injectedBySetterService.getMessage();
+	}
 }
